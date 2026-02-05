6370fbc3be1506808fb62bc7f43cadb848dd6595
CalebRose <ctrose17@gmail.com>
2026-02-05 08:22:48 -0800

    removing unused keys in config

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -7,6 +7,4 @@ export const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
-  simFBAKey: import.meta.env.VITE_SIMFBA_KEY,
-  simFBAUser: import.meta.env.VITE_SIMFBA_USER,
 };
