829ad9d75a9aaeeea9400502de5dc7a388f5f3d0
CalebRose <ctrose17@gmail.com>
2026-07-12 07:33:13 -0700

    url variable renames

--- src/_constants/urls.tsx
+++ src/_constants/urls.tsx
@@ -1,8 +1,8 @@
-const localURL = "http://localhost:5001/api/";
-const FBAApiURL = "https://simfba.azurewebsites.net/api/";
-export const fbaUrl = FBAApiURL;
+const fbaLocal = "http://localhost:5001/api/";
+const fbaApiURL = "https://simfba.azurewebsites.net/api/";
+export const fbaUrl = fbaApiURL;
 
-export const imagekitAuthUrl = `${FBAApiURL}imagekit/auth/`;
+export const imagekitAuthUrl = `${fbaApiURL}imagekit/auth/`;
 
 const localWSURL = "ws://localhost:5001/ws";
 const FBAWSURL = "wss://simfba.azurewebsites.net/ws";
@@ -12,12 +12,13 @@ const localBBAWSURL = "ws://localhost:8081/ws";
 const BBAWSURL = "wss://simnba.azurewebsites.net/ws";
 export const bba_ws = BBAWSURL;
 
-const local = "http://localhost:8081/api/";
-const apiURL = "https://simnba.azurewebsites.net/api/";
-export const bbaUrl = apiURL;
+const bbaLocal = "http://localhost:8081/api/";
+const bbaAPIUrl = "https://simnba.azurewebsites.net/api/";
+export const bbaUrl = bbaAPIUrl;
 
 const hckLocal = "http://localhost:8080/api/";
-const hckApiURL = "https://simhck-hqd2bme9gse5d7g9.westus-01.azurewebsites.net/api/";
+const hckApiURL =
+  "https://simhck-hqd2bme9gse5d7g9.westus-01.azurewebsites.net/api/";
 const localHckWSUrl = "ws://localhost:8080/ws";
 const HCKWSURL = "wss://simhck-hqd2bme9gse5d7g9.westus-01.azurewebsites.net/ws";
 
